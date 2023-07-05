3559a0e9322fb9a1a73f6ee5b7eb041e4ee4c710
Amit Vaghela <88328183+amit-s-vaghela@users.noreply.github.com>
2023-07-05 12:02:09 +0530

    Add files via upload

---
+++ MageMastery/BasicsViewModel/Controller/Post/Index.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace MageMastery\BasicsViewModel\Controller\Post;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    public function execute(): ResultInterface
+    {
+        
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
